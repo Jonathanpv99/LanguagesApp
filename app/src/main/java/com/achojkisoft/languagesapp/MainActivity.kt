8a5f589503f1ba6cafa6a54fb13f91e424b7b7ab
Jonathanpv99 <jonyvegapjv@gmail.com>
2025-04-01 12:07:54 -0600

    first commit

---
+++ app/src/main/java/com/achojkisoft/languagesapp/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.achojkisoft.languagesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.achojkisoft.languagesapp.ui.theme.LanguagesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LanguagesAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Surface (modifier = Modifier.fillMaxSize().systemBarsPadding()) {
+                        Greeting()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.title),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(id = R.string.subtitle),
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+}
